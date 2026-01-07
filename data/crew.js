675c6f16565f5d0514e1211a3fee2e92b20f04eb
SHREEHARI KALUNDIA <101623053+shreehari709@users.noreply.github.com>
2026-01-07 10:48:14 +0530

    Add files via upload

--- data/crew.js
+++ data/crew.js
@@ -3,12 +3,12 @@ export const crewMembers = [
     name: "Sunny Raj",
     role: "Lead Photographer",
     image:
-      "https://res.cloudinary.com/dq4l50k5a/image/upload/v1766935670/IMG_0801_pq1ndh.jpg",
+      "https://res.cloudinary.com/dq4l50k5a/image/upload/v1767756673/AKS04895_wuzjvv.jpg",
   },
   {
-    name: "Shreehari Kalundia",
-    role: "Cinematographer",
+    name: "Ajeet",
+    role: "Candid Photographer",
     image:
-      "https://res.cloudinary.com/dq4l50k5a/image/upload/v1766935671/IMG_6724_lcu7vl.jpg",
+      "https://res.cloudinary.com/dq4l50k5a/image/upload/v1767542036/DSC07050_hs7xvo.jpg",
   },
 ];
--- data/gallery.js
+++ data/gallery.js
@@ -1,23 +1,35 @@
 export const galleryItems = [
   {
+    id: "diana-edgar",
     title: "Diana & Edgar",
-    image:
-      "https://res.cloudinary.com/dq4l50k5a/image/upload/v1766682354/sw4_gngpww.jpg",
+    image: "https://res.cloudinary.com/dq4l50k5a/image/upload/v1766682354/sw4_gngpww.jpg",
   },
   {
+    id: "michelle-jake",
     title: "Michelle & Jake",
-    image:
-      "https://res.cloudinary.com/dq4l50k5a/image/upload/v1766682352/sw3_agjqqq.jpg",
+    image: "https://res.cloudinary.com/dq4l50k5a/image/upload/v1766682352/sw3_agjqqq.jpg",
   },
   {
+    id: "jon-jenny",
     title: "Jon & Jenny",
-    image:
-      "https://res.cloudinary.com/dq4l50k5a/image/upload/v1766682344/sw6_olqvhl.jpg",
+    image: "https://res.cloudinary.com/dq4l50k5a/image/upload/v1766682349/sw5_czsfc5.jpg",
   },
   {
+    id: "emma-lucas",
     title: "Emma & Lucas",
-    image:
-      "https://res.cloudinary.com/dq4l50k5a/image/upload/v1766682349/sw5_czsfc5.jpg",
+    image: "https://res.cloudinary.com/dq4l50k5a/image/upload/v1766682344/sw6_olqvhl.jpg",
+    blackWhite: true,
+  },
+  {
+    id: "emma",
+    title: "Emma",
+    image: "https://res.cloudinary.com/dq4l50k5a/image/upload/v1767756673/DSC09640_r8ni6q.jpg",
+    blackWhite: true,
+  },
+    {
+    id: "emma & john",
+    title: "Emma & John",
+    image: "https://res.cloudinary.com/dq4l50k5a/image/upload/v1767756672/DSC09436_copyyy_cvhmmh.jpg",
     blackWhite: true,
   },
 ];
