2a09370362e34d2ca176efb93cfb9338d03e5817
SHREEHARI KALUNDIA <101623053+shreehari709@users.noreply.github.com>
2026-02-08 18:56:10 +0530

    Update crew.js

--- data/crew.js
+++ data/crew.js
@@ -11,4 +11,10 @@ export const crewMembers = [
     image:
       "https://res.cloudinary.com/dq4l50k5a/image/upload/v1767542036/DSC07050_hs7xvo.jpg",
   },
+   {
+    name: "Pradeep",
+    role: "Cinematographer",
+    image:
+      "https://res.cloudinary.com/dq4l50k5a/image/upload/v1770556994/1000270786.jpg_sked0w.jpg",
+  },
 ];
